f1049754d3315694e5b7cfdb5ed3af2e2b97d3d3
Elias Grilo <114365954+eliasgrilo@users.noreply.github.com>
2024-03-06 23:16:06 -0700

    main

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tesla Clone - https://tesla-clone-yt-13a0a.web.app/
+
+Learn how to build the Tesla website using React and Tailwind CSS. YouTube Playlist [here](https://www.youtube.com/playlist?list=PL5ZlXxM-0LTEVCWlYYqIjPoFuq1a7bbVA).
+
+![Screenshot 2022-11-06 at 6 39 48 AM](https://user-images.githubusercontent.com/4500215/200174106-e5b68eb5-b200-4397-b657-07badd79632f.png)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Model from "./components/Model";
+
+function App() {
+  return (
+    <div className="bg-[url('https://ik.imagekit.io/tsadb415v5/22.mp4?updatedAt=1709057231909')] lg:bg-[url('https://ik.imagekit.io/tsadb415v5/Model-FY.mp4?updatedAt=1709077007973')] h-screen bg-cover bg-center">
+      <Navbar />
+      <Model />
+    </div>
+  );
+}
+
+export default App;
